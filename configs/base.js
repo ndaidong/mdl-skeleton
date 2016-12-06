07b187cd3853a6579858ba455a4ce3667f406d44
Dong Nguyen <ndaidong@gmail.com>
2016-12-06 17:27:40 +0700

    Update csp config

--- configs/base.js
+++ configs/base.js
@@ -30,11 +30,6 @@ config.port = 9999;
 
 config.settings = {
   dateformat: 'D, M d, Y h:i:s O',
-  memberOnlyRedirect: '/',
-  guestOnlyRedirect: '/',
-  defaultUserAvatar: '/images/icons/no-avatar.gif',
-  ticketDir: './storage/tickets/',
-  cacheDir: './storage/cache/',
   viewDir: './app/views/',
   tplFileExtension: '.html'
 };
@@ -45,22 +40,64 @@ config.staticData = {
   lastModified: true
 };
 
-/*eslint-disable */
 config.csp = {
-  defaultSrc: ["'self'"],
-  scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", '*.googleapis.com', '*.gstatic.com', '*.google-analytics.com'],
-  styleSrc: ["'self'", "'unsafe-inline'", '*.googleapis.com', '*.gstatic.com', '*.google-analytics.com'],
-  fontSrc: ["'self'", "'unsafe-inline'", 'data:', '*.googleapis.com', '*.gstatic.com', '*.google-analytics.com'],
-  imgSrc: ["'self'", 'data:', '*', '*.googleapis.com', '*.gstatic.com', '*.google-analytics.com'],
-  frameSrc: ["'self'"],
-  connectSrc: ["'self'"],
-  sandbox: ['allow-forms', 'allow-scripts', 'allow-popups', 'allow-same-origin', 'allow-top-navigation'],
-  reportUri: '/report-violation',
-  objectSrc: [],
+  directives: {
+    defaultSrc: [
+      `'self'`
+    ],
+    scriptSrc: [
+      `'self'`,
+      `'unsafe-inline'`,
+      `'unsafe-eval'`,
+      '*.googleapis.com',
+      '*.gstatic.com',
+      '*.google-analytics.com'
+    ],
+    styleSrc: [
+      `'self'`,
+      `'unsafe-inline'`,
+      '*.googleapis.com',
+      '*.gstatic.com',
+      '*.google-analytics.com'
+    ],
+    fontSrc: [
+      `'self'`,
+      `'unsafe-inline'`,
+      `data:`,
+      '*.googleapis.com',
+      '*.gstatic.com',
+      '*.google-analytics.com'
+    ],
+    imgSrc: [
+      `'self'`,
+      `data:`,
+      '*.googleapis.com',
+      '*.gstatic.com',
+      '*.google-analytics.com'
+    ],
+    frameSrc: [
+      `'self'`
+    ],
+    connectSrc: [
+      `'self'`
+    ],
+    sandbox: [
+      'allow-forms',
+      'allow-scripts',
+      'allow-popups',
+      'allow-same-origin',
+      'allow-top-navigation'
+    ],
+    objectSrc: [
+      `'none'`
+    ],
+    reportUri: '/report-violation'
+  },
   reportOnly: false,
   setAllHeaders: false,
   disableAndroid: false,
-  safari5: false
+  safari5: false,
+  loose: true
 };
 
 config.hsts = {
@@ -68,6 +105,5 @@ config.hsts = {
   includeSubDomains: true,
   force: true
 };
-/*eslint-enable */
 
 module.exports = config;
