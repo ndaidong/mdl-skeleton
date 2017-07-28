216808d9bc6f99e76196a1c71509b70fada3afeb
Dong Nguyen <ndaidong@gmail.com>
2017-07-28 15:20:27 +0700

    v2.2.1
    - Update dependencies
    - Use testcafe with phantomjs

--- test/start.js
+++ test/start.js
@@ -10,7 +10,7 @@ var test = require('tape');
 var dirs = ['', 'helpers', 'pages', 'serving'];
 
 dirs.forEach((dir) => {
-  let where = './test/specs/' + dir;
+  let where = './test/units/' + dir;
   if (fs.existsSync(where)) {
     fs.readdirSync(where).forEach((file) => {
       if (file.endsWith('.specs.js')) {
